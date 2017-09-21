74556c7faadd10958a600094c8bb080376ec4db0
tony <13717910964@163.com>
2017-09-21 17:03:02 +0800

    change github dir in dockerfile

--- fabfile.py
+++ fabfile.py
@@ -504,19 +504,19 @@ def download_model(root_dir, model_type_dir_name, model_dir_name, model_json):
     with lcd(model_type_dir):
         if not os.path.isdir(model_dir):
             os.mkdir(model_dir)
-            if sys.platform == 'darwin':
-                local("cd {} && cp /users/aub3/Dropbox/DeepVideoAnalytics/shared/{} .".format(model_dir_name, filename))
-            else:
-                local("cd {} && wget --quiet {}".format(model_dir_name, url))
+            #if sys.platform == 'darwin':
+            local("cd {} && cp /users/aub3/Dropbox/DeepVideoAnalytics/shared/{} .".format(model_dir_name, filename))
+            #else:
+            #    local("cd {} && wget --quiet {}".format(model_dir_name, url))
             if 'additional_files' in model_json:
                 for m in model_json["additional_files"]:
                     url = m['url']
                     filename = m['filename']
-                    if sys.platform == 'darwin':
-                        local("cd {} && cp /users/aub3/Dropbox/DeepVideoAnalytics/shared/{} .".format(model_dir_name,
+                    #if sys.platform == 'darwin':
+                    local("cd {} && cp /users/aub3/Dropbox/DeepVideoAnalytics/shared/{} .".format(model_dir_name,
                                                                                                       filename))
-                    else:
-                        local("cd {} && wget --quiet {}".format(model_dir_name, url))
+                    #else:
+                    #    local("cd {} && wget --quiet {}".format(model_dir_name, url))
 
 
 @task
