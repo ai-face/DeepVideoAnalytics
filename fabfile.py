151ce2eab1fdafd3e526569b1346fb22aa95b928
tony <13717910964@163.com>
2017-09-24 15:12:51 +0800

    add igiignore

--- fabfile.py
+++ fabfile.py
@@ -505,7 +505,7 @@ def download_model(root_dir, model_type_dir_name, model_dir_name, model_json):
         if not os.path.isdir(model_dir):
             os.mkdir(model_dir)
             #if sys.platform == 'darwin':
-            local("cd {} && cp /users/aub3/Dropbox/DeepVideoAnalytics/shared/{} .".format(model_dir_name, filename))
+            local("cd {} && cp /home/tom/ai/DeepVideoAnalytics.shared/models/{} .".format(model_dir_name, filename))
             #else:
             #    local("cd {} && wget --quiet {}".format(model_dir_name, url))
             if 'additional_files' in model_json:
@@ -513,7 +513,7 @@ def download_model(root_dir, model_type_dir_name, model_dir_name, model_json):
                     url = m['url']
                     filename = m['filename']
                     #if sys.platform == 'darwin':
-                    local("cd {} && cp /users/aub3/Dropbox/DeepVideoAnalytics/shared/{} .".format(model_dir_name,
+                    local("cd {} && cp /home/tom/ai/DeepVideoAnalytics.shared/models/{} .".format(model_dir_name,
                                                                                                       filename))
                     #else:
                     #    local("cd {} && wget --quiet {}".format(model_dir_name, url))
